bc08b0d4bb767e19a6d30c4782cfbf83fbbfed6a
jguas <jguaschllobera@gmail.com>
2024-07-03 01:59:06 +0200

    Commit undone and reverted to initial commit with original operations views and design changes

--- myfinanceops_frontend/src/views/LoginView.vue
+++ myfinanceops_frontend/src/views/LoginView.vue
@@ -1,25 +1,9 @@
 <template>
-  <nav class="py-10 px-8 border-b border-gray-200">
-    <div class="max-w-7xl mx-auto">
-      <div class="flex items-center justify-between">
-        <div class="menu-left">
-          <a href="#" class="text-xl">Wey</a>
-        </div>
-
-        <div class="menu-right">
-          <a href="#" class="py-4 px-6 bg-gray-600 text-white rounded-lg">Log in</a>
-          <a href="#" class="py-4 px-6 bg-purple-600 text-white rounded-lg">Sign up</a>
-        </div>
-      </div>
-    </div>
-  </nav>
-
   <main class="px-8 py-6 bg-gray-100">
     <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
-      <div class="main-left col-span-2">
-        <div class="p-12 bg-white border border-gray-200 rounded-lg">
+      <div class="main-left col-span-2 flex flex-col">
+        <div class="p-20 bg-white border border-gray-200 rounded-lg flex-1">
           <h1 class="mb-6 text-2xl">Log in</h1>
-
           <p class="mb-6 text-gray-500">
             <strong>Creating accounts is not supported on this application.</strong>
             <br>
@@ -28,9 +12,8 @@
           </p>
         </div>
       </div>
-
-      <div class="main-center col-span-2 space-y-4">
-        <div class="p-12 bg-white border border-gray-200 rounded-lg">
+      <div class="main-center col-span-2 space-y-4 flex flex-col">
+        <div class="p-20 bg-white border border-gray-200 rounded-lg flex-1">
           <form class="space-y-6">
             <div>
               <label>E-mail</label><br>
