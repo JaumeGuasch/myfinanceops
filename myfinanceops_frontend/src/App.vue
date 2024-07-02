1efcd4c5c815a403434fb6bc42e6f2ae47f7db5f
jguas <jguaschllobera@gmail.com>
2024-07-03 01:27:42 +0200

    First commit

--- myfinanceops_frontend/src/App.vue
+++ myfinanceops_frontend/src/App.vue
@@ -4,15 +4,13 @@
       <div class="flex items-center justify-between">
         <div class="menu-left">
           <a href="#" class="text-xl flex">
-            <img src="./assets/mainlogo.png" width="140">
-
+            <img src="./assets/mainlogo.png" width="120">
           </a>
         </div>
-
-        <div class="menu-center absolute left-1/2 transform -translate-x-1/2 flex space-x-12">
+        <div class="menu-center absolute left-1/2 transform -translate-x-1/2 flex space-x-20">
           <a href="#" class="flex flex-col items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                 stroke="currentColor" class="size-6">
+                 stroke="currentColor" class="size-8">
               <path stroke-linecap="round" stroke-linejoin="round"
                     d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
             </svg>
@@ -22,33 +20,39 @@
           <a href="#" class="flex flex-col items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor"
-                 class="w-6 h-6">
+                 class="w-8 h-8">
               <path stroke-linecap="round" stroke-linejoin="round"
                     d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
             </svg>
-            <span>Search</span>
+            <span>Hedging</span>
           </a>
 
           <a href="#" class="flex flex-col items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                 stroke="currentColor" class="size-6">
+                 stroke="currentColor" class="size-8">
               <path stroke-linecap="round" stroke-linejoin="round"
                     d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
             </svg>
-            <span>Operations</span>
+            <span>Trades</span>
           </a>
-
+          <a href="#" class="flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="size-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"/>
+              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"/>
+            </svg>
+            <span>Analytics</span>
+          </a>
           <a href="#" class="flex flex-col items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor"
-                 class="w-6 h-6">
+                 class="w-8 h-8">
               <path stroke-linecap="round" stroke-linejoin="round"
                     d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"/>
             </svg>
             <span>Notifications</span>
           </a>
         </div>
-
         <div class="menu-right">
           <a href="#">
             <img src="https://i.pravatar.cc/40?img=70" class="rounded-full" width="48">
@@ -57,8 +61,6 @@
       </div>
     </div>
   </nav>
-
-
   <main class="px-8 py-6 bg-gray-100">
     <RouterView/>
   </main>
---
+++ myfinanceops_frontend/src/views/HedgingView.vue
@@ -0,0 +1,32 @@
+<template>
+  <body>
+  <div class="max-w-5xlxl mx-auto px-48">
+    <div class="px-6 py-4 bg-white border border-gray-200 rounded-lg flex items-center justify-between">
+      <h1 class="text-xl">List of hedging operations</h1>
+      <div class="flex items-center">
+        <button class="py-2 px-4 bg-purple-600 text-white rounded-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+               stroke="currentColor"
+               class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"/>
+          </svg>
+        </button>
+        <button class="py-2 px-4 bg-purple-600 text-white rounded-lg ml-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+               stroke="currentColor"
+               class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="mb-1000-7xl mx-auto grid grid-cols-4 gap-4">
+  </div>
+  </body>
+</template>
+<style scoped>
+</style>
+<script setup lang="ts">
+</script>
